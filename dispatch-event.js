46d1a76c831a2c54c70fa055a28b79c8ef19d8ed
Joé Jemmely <joe.jemmely@gmail.com>
2019-06-05 10:20:12 -0700

    Fix error

--- dispatch-event.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = buffer => {
-  const text = buffer.toString().trim();
-  const [eventType, eventPayload] = text.split(':');
-  for (let socket in socketPool) {
-    socketPool[socket].write(`${eventType} ${eventPayload}`);
-  }
-};
